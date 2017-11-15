aaf6ece2e7ff6b8925b53a82bcfeaecb2b348cc6
RomanBresson <roman.bresson.5@gmail.com>
2017-11-16 00:08:09 +0100

    First working naive implementation - tests added

--- main.cpp
+++ main.cpp
@@ -14,16 +14,22 @@ int main() {
     Grid< std::uniform_real_distribution<float>, 3 > grid( bottom_left, top_right, 10 );
 
     //std::cout << grid << std::endl;
+    int nb_of_boids = 10000;
     std::vector<Boid <std::uniform_real_distribution<float>, 3> > vec;
-    for(int i=0; i<3; i++){
+    for(int i=0; i<nb_of_boids; i++){
         Boid<std::uniform_real_distribution<float>, 3> bob(bottom_left, top_right);
-        bob.cohesion_update(vec);
-        bob.separation_update(vec);
-        bob.alignment_update(vec);
-        bob.border_force_update(bottom_left, top_right);
+        bob.update_forces(vec, bottom_left, top_right);
+        bob.update_velocity(vec);
         std::cout << bob << std::endl;
         vec.push_back(bob);
     }
-
+    for(int j = 0; j<1000; j++){
+        for(int i=0; i<nb_of_boids; i++){
+            vec[i].update_forces(vec, bottom_left, top_right);
+            vec[i].update_velocity(vec);
+            std::cout << vec[i] << std::endl;
+        }
+        std::cout << "-------------------------------" << std::endl;
+    }
     return 0;
 }
