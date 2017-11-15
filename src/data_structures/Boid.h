133532410ffadc9c5ab76a4a8b0d4305e7068e5d
Adrien Suau <adrien.suau@grenoble-inp.org>
2017-11-15 19:41:25 +0100

    Review of the Boid class.
    1) Review of the Doxygen documentation:
       - Changes in the formatting
       - Comments changed and improved
    2) Implementation of 2 helpers methods:
       - is_visible
          -> return true when the current boid can see the boid given in
             parameter.
       - squared_euclidian_distance
          -> return the square of the euclidian distance between the current
             boid ans the one given in parameter.
    3) The attributes are now private.
    4) The constants are now constexpr and in MAJUSCULES.
    5) #include of cmath instead of math.h

--- src/data_structures/Boid.h
+++ src/data_structures/Boid.h
@@ -5,36 +5,35 @@
 #include <random>
 #include <string>
 #include <ostream>
-#include <math.h>
+#include <cmath>
 
 using types::PositionType;
 using types::Position;
 using types::Velocity;
 using types::Force;
+using types::DistanceType;
 
-const float cohesion_normaliser = 0.01;
-const float alignment_normaliser = 0.01;
-const float separation_normaliser = 0.01;
-const float separation_min_distance = 1.0;
+constexpr float COHESION_NORMALISER = 0.01;
+constexpr float ALIGNEMENT_NORMALISER = 0.01;
+constexpr float SEPARATION_NORMALISER = 0.01;
+constexpr float SEPARATION_MIN_DISTANCE = 1.0;
 
 
 /**
  * Struct that represents a boid agent.
- * @tparam Position     The spatial coordinates of the agent.
- * @tparam Velocity     The coordinates of the agent's velocity.
- * @tparam Force        The sum of all forces applied on the agent.
+ * @tparam Position     spatial coordinates of the agent.
+ * @tparam Velocity     coordinates of the agent's velocity.
+ * @tparam Force        sum of all forces applied on the agent.
  */
 template <typename Distribution, std::size_t Dimension>
-struct Boid {
+class Boid {
 
-    Position<Dimension> m_position;
-    Velocity<Dimension> m_velocity;
-    Force   <Dimension> m_force;
+public:
 
     /**
      * Constructor for the Boid class.
-     * @param bottom_left     The bottom-left corner of the space we want to simulate.
-     * @param top_right       The top-right corner of the space we want to simulate.
+     * @param bottom_left     bottom-left corner of the space we want to simulate.
+     * @param top_right       top-right corner of the space we want to simulate.
      */
     Boid(Position<Dimension> const & bottom_left,
          Position<Dimension> const & top_right)
@@ -50,58 +49,56 @@ struct Boid {
     }
 
     /**
-      *Computes the force of alignment applied on the boid, then updates the boid's force parameter
-      *@tparam neighbours : list of the boids that are close enough to the agent to apply the force
-      */
+     * Computes the force of alignment applied on the boid, then updates the boid's force parameter.
+     * @param neighbours list of the boids that are close enough to the agent to apply the force.
+     */
     void alignement_update  (const std::vector<Boid> & neighbours);
+
     /**
-      *Computes the force of cohesion applied on the boid, then updates the boid's force parameter
-      *@tparam neighbours : list of the boids that are close enough to the agent to apply the force
-      */
+     * Computes the force of cohesion applied on the boid, then updates the boid's force parameter.
+     * @param neighbours list of the boids that are close enough to the agent to apply the force.
+     */
     void cohesion_update(const std::vector<Boid> & neighbours) {
         if(neighbours.size() != 0){
             Position<Dimension> center = compute_center_of_mass(neighbours);
-            for(int j=0; j<Dimension; j++) {
-                m_force[j] += cohesion_normaliser*(center[j] - m_position[j]);
+            for(std::size_t j{0}; j < Dimension; ++j) {
+                m_force[j] += COHESION_NORMALISER*(center[j] - m_position[j]);
             }
         }
     }
     /**
-      *Computes the force of separation applied on the boid, then updates the boid's force parameter
-      *@tparam neighbours : list of the boids that are close enough to the agent to apply the force
-      */
+     * Computes the force of separation applied on the boid, then updates the boid's force parameter.
+     * @param neighbours list of the boids that are close enough to the agent to apply the force.
+     * @todo For the moment the force is linear. We probably want to change it to inverse of the distance between the
+     * two boids.
+     */
     void separation_update(const std::vector<Boid> & neighbours) {
         for(auto const & neighbour : neighbours) {
-            //TODO : JUST A NAIVE DISTANCE COMPUTING
-            float dist = 0.0;
-            for(int j=0; j<Dimension; j++) {
-                dist += pow(m_position[j]-neighbour.m_position[j], 2);
-            }
-            if (dist <= pow(separation_min_distance, 2)){
-                for(int j=0; j<Dimension; j++) {
-                    m_force[j] += separation_normaliser*(m_position[j]-neighbour.m_position[j]);
+            if(is_visible(neighbour)) {
+                for(std::size_t j{0}; j < Dimension; ++j) {
+                    m_force[j] += SEPARATION_NORMALISER*(m_position[j]-neighbour.m_position[j]);
                 }
             }
         }
     }
     /**
-      *Computes the force of repulsion applied on the boid by the borders of the grid, then updates the boid's force parameter
-      *@tparam neighbours : list of the boids that are close enough to the agent to apply the force
-      */
-    void border_force_update();
+     * Computes the force of repulsion applied on the boid by the borders of the grid, then updates the boid's
+     * force parameter.
+     * @param neighbours list of the boids that are close enough to the agent to apply the force.
+     * @todo For the moment the force is linear. We probably want to change it to inverse of the distance between the
+     * two boids.
+     */
+    void border_force_update(Position<Dimension> const & bottom_left, Position<Dimension> const & top_right);
 
     /**
-      *Computes the center of mass of a number of boids
-      *@tparam neighbours : list of the boids whose center is to be computed
-      */
+     * Computes the center of mass of a number of boids
+     * @param neighbours list of the boids whose center is to be computed
+     */
     Position<Dimension> compute_center_of_mass(const std::vector<Boid> & neighbours) {
-        Position<Dimension> center;
-        for (int j=0; j<Dimension; j++) {
-                center[j] =0.0;
-        }
-        if (neighbours.size() ==0){ //no neighbours are perceived : no influence must be had on our current boid
+        if (neighbours.size() == 0){ // no neighbours are perceived : no influence must be had on our current boid
             return(Position<Dimension>{-1});
         }
+        Position<Dimension> center{};
         for(auto const & neighbour : neighbours) {
             for (int j=0; j<Dimension; j++) {
                 center[j] += neighbour.m_position[j]/neighbours.size();
@@ -110,6 +107,41 @@ struct Boid {
         }
         return center;
     }
+
+private:
+
+    /**
+     * Tell wether or not a given boid is visible by the current instance.
+     * @tparam D   probability distribution of the other boid.
+     * @param boid a boid.
+     * @return     true if the given boid is visible by the current instance, false otherwise.
+     */
+    template <typename D>
+    bool is_visible(const Boid<D, Dimension> & boid) {
+        DistanceType const squared_distance{ this->squared_euclidian_distance(boid) };
+        return squared_distance <= SEPARATION_MIN_DISTANCE * SEPARATION_MIN_DISTANCE;
+    }
+
+    /**
+     * Compute the (squared) euclidian distance between the current instance and the given boid.
+     * @tparam D   probability distribution of the other boid.
+     * @param boid the other boid.
+     * @return     distance between the two boids.
+     */
+    template <typename D>
+    DistanceType squared_euclidian_distance(const Boid<D, Dimension> & boid) {
+        DistanceType distance{DistanceType()};
+        for(std::size_t i{0}; i < Dimension; ++i) {
+            const PositionType diff{m_position[i] - boid.m_position[i]};
+            distance += diff*diff;
+        }
+        return distance;
+    }
+
+    Position<Dimension> m_position;
+    Velocity<Dimension> m_velocity;
+    Force   <Dimension> m_force;
+
 };
 
 template <typename Distribution, std::size_t Dimension>
