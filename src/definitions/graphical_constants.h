9783638357436de988b670b8341b68d066bd9467
Adrien Suau <adrien.suau@grenoble-inp.org>
2017-11-16 20:49:46 +0100

    Added constants for the visualization part.

--- src/definitions/graphical_constants.h
+++ src/definitions/graphical_constants.h
@@ -1,6 +1,14 @@
 #ifndef SWARMING_PROJECT_GRAPHICAL_CONSTANTS_H_H
 #define SWARMING_PROJECT_GRAPHICAL_CONSTANTS_H_H
 
-constexpr float 
+#include <cstddef>
 
+namespace gconst {
+
+    constexpr const float BOID_RADIUS{0.01};
+    constexpr const std::size_t BOID_NUMBER_OF_SIDES{10};
+    constexpr const std::size_t VTK_COORDINATES_NUMBER{3};
+    constexpr const std::size_t UPDATE_DELAY_MS{10000};
+
+}
 #endif //SWARMING_PROJECT_GRAPHICAL_CONSTANTS_H_H
