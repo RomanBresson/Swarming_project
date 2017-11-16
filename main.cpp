ad3485654d76c83a676f51f697484c5e908c22ee
Roman Bresson <roman.bresson@ensimag.grenoble-inp.fr>
2017-11-16 14:36:32 +0100

    test FOV

--- main.cpp
+++ main.cpp
@@ -6,30 +6,38 @@
 #include <random>
 
 using types::Position;
-using Vector3Pos = Position<3>;
+using Vector3Pos = Position<2>;
 
 int main() {
-    Vector3Pos bottom_left({0.0, 0.0, 0.0});
-    Vector3Pos top_right({10.0, 10.0, 10.0});
-    Grid< std::uniform_real_distribution<float>, 3 > grid( bottom_left, top_right, 10 );
+    const int nb_of_boids = 5;
+    Vector3Pos bottom_left({0.0, 0.0});
+    Vector3Pos top_right({10.0, 10.0});
+    Grid< std::uniform_int_distribution<int>, 2 > grid( bottom_left, top_right, nb_of_boids );
 
-    //std::cout << grid << std::endl;
-    int nb_of_boids = 10000;
-    std::vector<Boid <std::uniform_real_distribution<float>, 3> > vec;
+    std::cout << grid << std::endl;
     for(int i=0; i<nb_of_boids; i++){
-        Boid<std::uniform_real_distribution<float>, 3> bob(bottom_left, top_right);
-        bob.update_forces(vec, bottom_left, top_right);
-        bob.update_velocity(vec);
-        std::cout << bob << std::endl;
-        vec.push_back(bob);
-    }
+        for(int j=0; j<nb_of_boids; j++){
+            if (i!=j){
+                if (grid.m_boids[i].is_visible(grid.m_boids[j])){
+                    std::cout << grid.m_boids[i] << "   CAN SEE   " << grid.m_boids[j] << std::endl;
+                } else {
+                    std::cout << grid.m_boids[i] << "   CANNOT SEE   " << grid.m_boids[j] << std::endl;
+                }
+            }
+        }
+        //Boid<std::uniform_real_distribution<float>, 3> bob(bottom_left, top_right);
+        //bob.update_forces(vec, bottom_left, top_right);
+        //bob.update_velocity(vec);
+        //std::cout << bob << std::endl;
+        //vec.push_back(bob);
+    }/*
     for(int j = 0; j<1000; j++){
         for(int i=0; i<nb_of_boids; i++){
             vec[i].update_forces(vec, bottom_left, top_right);
             vec[i].update_velocity(vec);
-            std::cout << vec[i] << std::endl;
+            //std::cout << vec[i] << std::endl;
         }
-        std::cout << "-------------------------------" << std::endl;
-    }
+        //std::cout << "-------------------------------" << std::endl;
+    }*/
     return 0;
 }
