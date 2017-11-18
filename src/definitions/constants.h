93888e6bbacfcc0d55b4d23cade4a9dccf3a9dd7
RomanBresson <roman.bresson.5@gmail.com>
2017-11-18 11:43:10 +0100

    Constants changed for empirical realism

--- src/definitions/constants.h
+++ src/definitions/constants.h
@@ -6,13 +6,13 @@ namespace constants {
     constexpr const double DOUBLE_EPSILON{1e-10};
 
     constexpr const double PI{3.14159265};
-    constexpr const float COHESION_NORMALISER = 0.01;
+    constexpr const float COHESION_NORMALISER = 0.1;
     constexpr const float ALIGNMENT_NORMALISER = static_cast<float>(1.0/8.0);
-    constexpr const float SEPARATION_NORMALISER = 0.1;
+    constexpr const float SEPARATION_NORMALISER = 1;
     constexpr const float BORDER_SEPARATION_NORMALISER = 1.0;
 
     constexpr const float VISION_DISTANCE = 5.0;
-    constexpr const float VISION_ANGLE = 90.0; //In degrees
+    constexpr const float VISION_ANGLE = 120.0; //In degrees
 
     constexpr const float SEPARATION_MIN_DISTANCE = 1;
     constexpr const float REPULSION_DISTANCE = 1.0;
