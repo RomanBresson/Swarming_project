959fa8ca093b95c9bdaf16dc1df304be6f2f4317
Nelimee <adrien.suau@grenoble-inp.org>
2018-01-06 15:23:49 +0100

    Revert some commit to program with MPI on an other branch.

--- main.cpp
+++ main.cpp
@@ -1,19 +1,11 @@
 #include <iostream>
 #include "data_structures/Octree.h"
-#include "mpi.h"
-#include "data_structures/Boid.h"
 #include "data_structures/Grid.h"
 #include "visualization/GridVisualizer.h"
 
 using types::Position;
 
 int main(int argc, char *argv[]) {
-
-    // Initialise MPI.
-    MPI_Init(&argc, &argv);
-    int process_number, process_ID;
-    MPI_Comm_size(MPI_COMM_WORLD, &process_number);
-    MPI_Comm_rank(MPI_COMM_WORLD, &process_ID);
 
     // Initialise the constants of the program.
     constexpr const std::size_t DIMENSION{2};
@@ -43,17 +35,8 @@ int main(int argc, char *argv[]) {
     Grid<std::uniform_real_distribution<float>, DIMENSION> grid(bottom_left, top_right, NUMBER_OF_BOIDS);
     std::cout << "Done!" << std::endl;
 
-//    GridVisualizer<std::uniform_real_distribution<float>, DIMENSION> visualizer(grid);
-//
-//    visualizer.start();
-
-    for (std::size_t i{0}; i < 1000; ++i) {
-        std::cout << "Update n°" << i << "... " << std::flush;
-        grid.update_all_boids();
-        std::cout << "Done!" << std::endl;
-    }
-
-    MPI_Finalize();
+    GridVisualizer<std::uniform_real_distribution<float>, DIMENSION> visualizer(grid);
+    visualizer.start();
 
     return 0;
 
