358e0954aac2564f7647360bdd6ecfb8d52ed36e
Nelimee <adrien.suau@grenoble-inp.org>
2018-01-06 11:18:25 +0100

    Computations and visualization in 2D.
    Also a small re-indentation in the CMakeLists.txt

--- main.cpp
+++ main.cpp
@@ -3,13 +3,13 @@
 #include "data_structures/Grid.h"
 #include "visualization/GridVisualizer.h"
 
-constexpr const std::size_t DIMENSION{3};
+constexpr const std::size_t DIMENSION{2};
 constexpr const std::size_t NUMBER_OF_BOIDS{500};
 using types::Position;
 
 int main() {
-    Position<DIMENSION> bottom_left({0.0, 0.0, 0.0});
-    Position<DIMENSION> top_right({100.0, 100.0, 100.0});
+    Position<DIMENSION> bottom_left({0.0, 0.0});
+    Position<DIMENSION> top_right({100.0, 100.0});
 
     Grid<std::uniform_real_distribution<float>, DIMENSION> grid(bottom_left, top_right, NUMBER_OF_BOIDS);
     GridVisualizer<std::uniform_real_distribution<float>, DIMENSION> visualizer(grid);
